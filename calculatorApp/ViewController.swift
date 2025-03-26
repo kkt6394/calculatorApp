2b65d343276c1bfb20ccb11b803b0bee4c316da8
Aiden <kkt6394@gmail.com>
2025-03-26 11:41:22 +0900

    main/feat/UILabel(lv1)

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    private func configureUI() {
+        view.backgroundColor = .black
+        label.textColor = .white
+        label.text = "12345"
+        label.textAlignment = .right
+        label.font = .boldSystemFont(ofSize: 60)
+        
+        [label]
+            .forEach {view.addSubview($0)}
+        
+        label.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(200)
+            $0.height.equalTo(100)
+        }
     }
 
 
